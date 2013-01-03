9b777433d8f490350c025765a6c0763764214946
mantou <mantoumail-16@yahoo.com.cn>
2013-01-03 22:17:25 +0800

    modify "qire", change the code according to the change of site: the first page no longer get data through XHR.

--- RateListPy/QireRate.py
+++ RateListPy/QireRate.py
@@ -1,3 +1,5 @@
+# -*- coding: UTF-8 -*-
+
 '''
 Created on Oct 20, 2012
 
@@ -22,8 +24,7 @@ class QireRate:
     
     def __init__(self, base_url, file_name):
         self.__base_url = base_url
-        self.__qire_header = self.__set_qire_header()
-        self.__douban_header = self.__set_douban_header()
+        self.__header = self.set_header()
         try:
             self.__rate_file = codecs.open(file_name, 'w', 'utf-8')
             self.__rate_file.write(codecs.BOM_UTF8)
@@ -47,53 +48,25 @@ class QireRate:
 
     # this all to make sure we can get the right response from server
     # set host, referer incase there is some anti robot crawling rules running on server
-    def __set_qire_header(self):
-        httpheader = dict()
-        httpheader['Accept']='application/json, text/javascript, */*; q=0.01'
-        httpheader['Accept-Charset']='GBK,utf-8;q=0.7,*;q=0.3'
-        httpheader['Accept-Encoding']='gzip,deflate,sdch'
-        httpheader['Accept-Language']='en-US,en;q=0.8,zh-CN;q=0.6,zh;q=0.4'
-        httpheader['Host']='www.qire123.net'
-        httpheader['Proxy-Connection']='keep-alive'
-        httpheader['Referer']='http//www.qire123.net/vod-showlist-id-8-order-hits.html'
+    def set_header(self):
+        header = dict()
+        header['Accept']='application/json, text/javascript, */*; q=0.01'
+        header['Accept-Charset']='GBK,utf-8;q=0.7,*;q=0.3'
+        header['Accept-Encoding']='gzip,deflate,sdch'
+        header['Accept-Language']='en-US,en;q=0.8,zh-CN;q=0.6,zh;q=0.4'
+        header['Host']='www.qire123.com'
+        header['Proxy-Connection']='keep-alive'
+        header['Referer']='http://www.qire123.com/vod-showlist-id-8-order-hits.html'
         # remove below line because somehow can't make the conntion with it
-        #httpheader['User-Agent']='Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.4 (KHTML, like Gecko) Chrome/22.0.1229.94 Safari/537.4'
-        httpheader['X-Requested-With']='XMLHttpRequest'  #this ensure we make the request through ajax and can get the json data back from it
+        #header['User-Agent']='Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.4 (KHTML, like Gecko) Chrome/22.0.1229.94 Safari/537.4'
+        header['X-Requested-With']='XMLHttpRequest'  #this ensure we make the request through ajax and can get the json data back from it
         
-        return httpheader
-    
-    def __set_douban_header(self):
-        httpheader = dict()
-        httpheader['Accept']='application/json, text/javascript, */*; q=0.01'
-        httpheader['Accept-Charset']='GBK,utf-8;q=0.7,*;q=0.3'
-        httpheader['Accept-Encoding']='gzip,deflate,sdch'
-        httpheader['Accept-Language']='en-US,en;q=0.8,zh-CN;q=0.6,zh;q=0.4'
-        httpheader['Host']='movie.douban.com'
-        httpheader['Proxy-Connection']='keep-alive'
-        httpheader['Referer']='http://movie.douban.com/'
-        # remove below line because somehow can't make the conntion with it
-        #httpheader['User-Agent']='Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.4 (KHTML, like Gecko) Chrome/22.0.1229.94 Safari/537.4'
-        httpheader['X-Requested-With']='XMLHttpRequest'
-        
-        return httpheader
-        
-    def get_nextpage(self, htmlCode):
-        # the htmlCode has the Chinese encoded like \u3a0d
-        # by doing below json loads, it turns back to Chinese
-        pages = json.loads(htmlCode)['pages'] #get value of key 'pages'
-        soup = BeautifulSoup(pages)
-        # find all tags with tag name 'a' and class = 'next pagegbk', next page
-        nextpage_tag = soup.find('a', {'class':'next pagegbk'})
-        if nextpage_tag != None:
-            nextpage_link = nextpage_tag['href'] # get link address
-        else:
-            nextpage_link = None
-        return nextpage_link
-        
-    def get_XHR_response(self, url, header):
+        return header
+
+    def get_XHR_response(self, url):
         req = urllib2.Request(url)
         # initial request header by header dict
-        for key, val in header.items():
+        for key, val in self.__header.items():
             req.add_header(key, val)
         res = urllib2.urlopen(req)
         headerObject = res.headers
@@ -114,62 +87,110 @@ class QireRate:
                 htmlCode = streamData
         else:
             htmlCode = streamData
-            if headerObject.has_key('Content-Type'):
-                contentType = headerObject['Content-Type']
-                if contentType.lower().find('charset=') != -1:
-                    charset = re.search(r'charset=([^;]*)', contentType.lower()).group(1)  
-                    if charset != 'utf-8':
-                        try:
-                            htmlCode = htmlCode.decode(charset).encode('utf-8')
-                        except:
-                            pass
+
+        if headerObject.has_key('Content-Type'):
+            contentType = headerObject['Content-Type']
+            if contentType.lower().find('charset=') != -1:
+                charset = re.search(r'charset=([^;]*)', contentType.lower()).group(1)  
+                if charset != 'utf-8':
+                    try:
+                        htmlCode = htmlCode.decode(charset).encode('utf-8')
+                    except:
+                        pass
         res.close()
-        print htmlCode
         return htmlCode
-    
-    def fetch_rate(self, url):
-        qire_html_res = self.get_XHR_response(url, self.__qire_header)
+
+    def get_link_tag(self, tag):
+        return tag.contents[0]
+    def get_rate(self, tag):
+        return tag.findNextSiblings('p', {'class':'count'})[0].find('strong', {'class':'ratbar-num'}).string
+    def get_name(self, tag):
+        return tag.string
+    def get_link_url(self, tag):
+        return tag['href']
+    def get_video_format(self, tag):
+        return tag.findNextSiblings('p', {'class':'state'})[0].find('a', {'class':'goplay'}).string
+    def get_current_page_id(self, soup):
+        return soup.find('span', {'class':'current'}).string
+    def get_nextpage_url(self, soup):
+        # the htmlCode has the Chinese encoded like \u3a0d
+        # by doing below json loads, it turns back to Chinese
+        #pages = json.loads(htmlCode)['pages'] #get value of key 'pages'
+        #soup = BeautifulSoup(pages)
+        # find all tags with tag name 'a' and class = 'next pagegbk', next page
+        nextpage_tag = soup.find('a', {'class':'next pagegbk'})
+        if nextpage_tag != None:
+            nextpage_link = self.__base_url + nextpage_tag['href'] # get link full address
+        else:
+            nextpage_link = None
+        return nextpage_link
+        
+    def parse_html(self, url):
+        req = urllib2.Request(url)
+        res = urllib2.urlopen(req)
+        html= res.read()
+        res.close()
+        soup = BeautifulSoup(html)
+        category = soup.find(id = "byletter")['href'].strip('/')
+        for tag in soup.findAll('h5'):
+            rate = self.get_rate(tag)
+            link_tag = self.get_link_tag(tag)
+            name = self.get_name(link_tag)
+            link = self.get_link_url(link_tag)
+            video_format = self.get_video_format(tag)
+            self.save_to_file(category, '1', rate, name, link, video_format)
+            self.save_to_db(category, '1', rate, name, link, video_format)
+
+        nextpage_url = self.get_nextpage_url(soup)
+        if nextpage_url != None:
+            self.fetch_info(nextpage_url)
+            
+    def parse_json(self, url):
+        json_res = self.get_XHR_response(url)
         # get category name, from key 'letterurl'. orignal value is /action/
-        category = '%s' %json.loads(qire_html_res)['letterurl']
-        category = category.strip('/')
-        pages = json.loads(qire_html_res)['pages']
+        category = '%s' %json.loads(json_res)['letterurl'].strip('/')
+        pages = json.loads(json_res)['pages']
         soup = BeautifulSoup(pages)
-        current_page_id = soup.find('span', {'class':'current'}).string
+        current_page_id = self.get_current_page_id(soup)
         print "working on page: ", current_page_id
+        nextpage_url = self.get_nextpage_url(soup)
         # the major part of json data contains all the video information
-        ajaxtxt = json.loads(qire_html_res)['ajaxtxt']
+        ajaxtxt = json.loads(json_res)['ajaxtxt']
         soup = BeautifulSoup(ajaxtxt)
         for tag in soup.findAll('h5'):
-            link_tag = tag.contents[0]
-            score_tag = tag.findNextSiblings('p', {'class':'count'})[0].find('strong', {'class':'ratbar-num'})
-            video_tag = tag.findNextSiblings('p', {'class':'state'})[0].find('a', {'class':'goplay'})
-            record = category + ',' + current_page_id + ',' + score_tag.string + ',' + link_tag.string + ',' + link_tag['href'] + ',' + video_tag.string
-            douban_url = 'http://movie.douban.com/j/subject_suggest?q='+urllib2.quote(link_tag.string.encode('utf-8'))
-            douban_html_res = json.loads(self.get_XHR_response(douban_url, self.__douban_header))
-            if (len(douban_html_res) > 0):
-                douban_url = '%s' %douban_html_res[0]['url'].replace('\\', '')
-                douban_page = urllib2.urlopen(douban_url)
-                douban_soup = BeautifulSoup(douban_page,fromEncoding="utf-8")
-                douban_tag = douban_soup.find('strong', {'class':'ll rating_num'})
-                if douban_tag != None:
-                    record = record + ',' + '%s' %douban_tag.string
-            else:
-                print 'can not find ' + link_tag.string + ' on Douban'
-            self.__rate_file.write(record + '\n')
-            try:
-                self.__cursor.execute("insert into qire_rate (category, page_id, rate, name, link, format) values (%s, %s, %s, %s, %s, %s)", (category, current_page_id, score_tag.string, link_tag.string, link_tag['href'], video_tag.string))
-            except MySQLdb.Error, e:
-                print 'Error %d: %s' % (e.args[0], e.args[1])
-                print "this is the record making trouble:  ", record
-                
+            rate = self.get_rate(tag)
+            link_tag = self.get_link_tag(tag)
+            name = self.get_name(link_tag)
+            link = self.get_link_url(link_tag)
+            video_format = self.get_video_format(tag)
+            self.save_to_file(category, current_page_id, rate, name, link, video_format)
+            self.save_to_db(category, current_page_id, rate, name, link, video_format)
+        
         # recursively get the data if it is not last page
-        nextpage = self.get_nextpage(qire_html_res)
-        self.get_nextpage(qire_html_res)
-        if nextpage != None:
-            nextpage = self.__base_url + nextpage
+        if nextpage_url != None:# if nextpage == None, means this is the last page, so return
             # self call to do recursively fetch
-            self.fetch_rate(nextpage)
-        # if nextpage == None, means this is the last page, so return
+            self.parse_json(nextpage_url)
+            
+    def fetch_info(self, url):
+        if url.find("order-hits.html") > 0:
+            self.parse_html(url)
+        else:
+            self.parse_json(url)
+
+    
+    def save_to_file(self, category, page_id, rate, name, link, video_format):
+        record = category + ',' + page_id + ',' + rate + ',' + name + ',' + link + ',' + video_format
+        try:
+            self.__rate_file.write(record + '\n')
+        except:
+            print 'Having trouble to save this record to file: ', record
+        
+    def save_to_db(self, category, page_id, rate, name, link, video_format):
+        try:
+            self.__cursor.execute("insert into qire_rate (category, page_id, rate, name, link, format) values (%s, %s, %s, %s, %s, %s)", (category, page_id, rate, name, link, video_format))
+        except MySQLdb.Error, e:
+            print 'Error %d: %s' % (e.args[0], e.args[1])
+            print "this is the record making trouble:  ", category + ',' + page_id + ',' + rate + ',' + name + ',' + link + ',' + video_format
 
 if __name__ == '__main__':
     
@@ -180,7 +201,7 @@ if __name__ == '__main__':
     for category_id in range(8, 9):
         print 'working on category: ', category_id
         start_url = base_url + "/vod-showlist-id-" + str(category_id) + "-order-hits.html"
-        rate.fetch_rate(start_url)
+        rate.fetch_info(start_url)
     print 'task ends!'
         
     
