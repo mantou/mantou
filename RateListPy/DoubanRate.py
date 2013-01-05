ab2bb81decdebc11f1e1c5d5b132e9965198f7ca
mantou <mantoumail-16@yahoo.com.cn>
2013-01-05 23:10:53 +0800

    1. enable all the agents 2. add timestamp at begin and end of job

--- RateListPy/DoubanRate.py
+++ RateListPy/DoubanRate.py
@@ -50,12 +50,12 @@ class DoubanRate:
         user_agents = [
                     'Mozilla/5.0 (Windows; U; Windows NT 5.1; it; rv:1.8.1.11) Gecko/20071127 Firefox/2.0.0.11',
                     'Opera/9.25 (Windows NT 5.1; U; en)',
-                    #'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)',
-                    #'Mozilla/5.0 (compatible; Konqueror/3.5; Linux) KHTML/3.5.5 (like Gecko) (Kubuntu)',
-                    #'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.0.12) Gecko/20070731 Ubuntu/dapper-security Firefox/1.5.0.12',
-                    #'Lynx/2.8.5rel.1 libwww-FM/2.14 SSL-MM/1.4.1 GNUTLS/1.2.9',
-                    #'Mozilla/5.0 (X11; Linux i686) AppleWebKit/535.7 (KHTML, like Gecko) Ubuntu/11.04 Chromium/16.0.912.77 Chrome/16.0.912.77 Safari/535.7',
-                    #'Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:10.0) Gecko/20100101 Firefox/10.0',
+                    'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)',
+                    'Mozilla/5.0 (compatible; Konqueror/3.5; Linux) KHTML/3.5.5 (like Gecko) (Kubuntu)',
+                    'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.0.12) Gecko/20070731 Ubuntu/dapper-security Firefox/1.5.0.12',
+                    'Lynx/2.8.5rel.1 libwww-FM/2.14 SSL-MM/1.4.1 GNUTLS/1.2.9',
+                    'Mozilla/5.0 (X11; Linux i686) AppleWebKit/535.7 (KHTML, like Gecko) Ubuntu/11.04 Chromium/16.0.912.77 Chrome/16.0.912.77 Safari/535.7',
+                    'Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:10.0) Gecko/20100101 Firefox/10.0',
                     'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.97 Safari/537.11'
                     ]
         header['User-Agent'] = random.choice(user_agents)
@@ -208,9 +208,9 @@ if __name__ == '__main__':
     
     base_url = "http://movie.douban.com/tag/"
     rate = DoubanRate(base_url)
-    print 'task starts!'
+    print 'task starts: ', time.asctime(time.localtime())
     rate.fetch_info()
     #rate.parse_page('http://movie.douban.com/tag/%E7%A7%91%E5%B9%BB?start=100&type=T')
-    print 'task ends!'
+    print 'task ends: ', time.asctime(time.localtime())
 
         
